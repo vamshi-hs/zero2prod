3b77c5ba491daa5494fa4e52ec787f40581c2e3b
vamshi krishna <vamshi5070k@gmail.com>
2024-03-27 23:41:34 +0530

    Implement 02prod till 3.6 refocus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port.");
+ 
+    run(listener)?.await
 }
